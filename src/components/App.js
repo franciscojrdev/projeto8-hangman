2f0e9d35a2d16f716e94d02a09a3425cbc188b9b
franciscojrdev <franciscojunior7222@gmail.com>
2022-09-22 00:35:37 -0300

    layout base

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
+import forca0 from "../assets/img/forca0.png";
+import forca1 from "../assets/img/forca1.png";
+import forca2 from "../assets/img/forca2.png";
+import forca3 from "../assets/img/forca3.png";
+import forca4 from "../assets/img/forca4.png";
+import forca5 from "../assets/img/forca5.png";
+import forca6 from "../assets/img/forca6.png";
+
 export default function App() {
-  return <>
-    <h1>Teste de cabeçario</h1>
-    <button>Click</button> 
-  
-  </>;
+  return (
+    <>
+      <main>
+        <div className="forca">
+          <img src={forca6} />
+          <button>Escolher Palavra</button>
+        </div>
+      </main>
+    </>
+  );
 }
