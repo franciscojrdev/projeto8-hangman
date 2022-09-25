9135b7de81efd1d44929cae1beb5d6e0d3ac397c
franciscojrdev <franciscojunior7222@gmail.com>
2022-09-25 16:40:06 -0300

    base

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,58 @@ import forca4 from "../assets/img/forca4.png";
 import forca5 from "../assets/img/forca5.png";
 import forca6 from "../assets/img/forca6.png";
 
+const alfabeto = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+let images = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
 export default function App() {
   return (
     <>
       <main>
         <div className="forca">
           <img src={forca6} />
-          <button>Escolher Palavra</button>
+          <aside>
+            <button>Escolher Palavra</button>
+            <h1>Texto</h1>
+          </aside>
+        </div>
+        <div className="typeArea">
+          <ul>
+            {alfabeto.map((el) => (
+              <li>{el.toUpperCase()}</li>
+            ))}
+          </ul>
+          <div className="answear">
+            <label>Já sei a palavra!</label>
+            <input type="text"></input>
+            <button>Chutar</button>
+          </div>
         </div>
       </main>
     </>
