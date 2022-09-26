76575af4b3d95f971bad10eb7cb4696f2c0049b8
franciscojrdev <franciscojunior7222@gmail.com>
2022-09-26 00:26:07 -0300

    some alterations

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import forca3 from "../assets/img/forca3.png";
 import forca4 from "../assets/img/forca4.png";
 import forca5 from "../assets/img/forca5.png";
 import forca6 from "../assets/img/forca6.png";
+import words from "../data/palavras"
+import { useState } from "react";
 
 const alfabeto = [
   "a",
@@ -35,22 +37,66 @@ const alfabeto = [
   "z",
 ];
 
+function HideLetters(){
+  return(
+    <li>_</li>
+  )
+}
+let cont = 0;
 let images = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
 export default function App() {
+  const [image,setImage] =useState(images[cont]);
+  const [word, setWord] =useState([]);
+  const [clicked, setClicked] = useState([]);
+  const [haveLetter, setHaveLetter] = useState([]);
+  const [noLetter, setNoLetter] = useState([]);
+  const [endGame,setEndGame] = useState(false)
+
+
+  // let newArray = [...word];
+
+  function chooseWord(){
+    let position = words[Math.floor(Math.random() * words.length)].split("")
+    setWord(position)
+  }
+  function saveLetter(letter){
+    setClicked([...clicked,letter])
+    let compair = word.includes(letter)
+    if(compair){
+      setHaveLetter([...haveLetter,letter])
+      // alert('sim')
+    }else{
+      cont = cont + 1
+      let imgChoosed = images[cont];
+      setImage(imgChoosed)
+      setNoLetter([...noLetter,letter])
+      if(cont === 6){
+        setEndGame(true)
+      }
+      // alert('no')
+    }
+  }
+
+
+
+
   return (
     <>
       <main>
         <div className="forca">
-          <img src={forca6} />
+          <img src={image} />
           <aside>
-            <button>Escolher Palavra</button>
-            <h1>Texto</h1>
+            <button onClick={chooseWord}>Escolher Palavra</button>
+            <ul>
+
+              {!endGame? word.map((el,index) => <HideLetters key={index} />):<li className="color">Quarentena</li>}
+            </ul>
           </aside>
         </div>
         <div className="typeArea">
           <ul>
-            {alfabeto.map((el) => (
-              <li>{el.toUpperCase()}</li>
+            {alfabeto.map((el,index) => (
+              <li key={index} onClick={word.length!==0 ? ()=>saveLetter(el):() => console.warn("not yet")}>{el.toUpperCase()}</li>
             ))}
           </ul>
           <div className="answear">
